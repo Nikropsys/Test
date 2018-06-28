254cf2c0b45b7efc16521a4ceb1418f380c633d2
Andrey Godlevskiy <darkday8@bigmir.net>
2018-06-28 17:49:02 +0300

    add  test script

---
+++ New Unity Project (2)/Assets/Script/newscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void Metod1()
+    {
+
+    }
+    void Metod2()
+    {
+
+    }
+}
